755dcdd91954626ff9a139f1902ab81eb1c83bc6
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-08-21 21:56:39 +0900

    ディレクトリ構成修正

--- src/app/providers/router/routerConfig.tsx
+++ src/app/providers/router/routerConfig.tsx
@@ -1,18 +1,7 @@
-import { HomePage } from '@/pages/Home/ui/HomePage';
-import { LoginPage } from '@/pages/Login/ui/LoginPage';
+import { HomePage } from '@/pages/Home';
+import { LoginPage } from '@/pages/Login';
+import { AppRoutes, routePaths } from '@/shared/config';
 import type { RouteProps } from 'react-router';
-
-const AppRoutes = {
-  HOME: 'home',
-  LOGIN: 'login',
-} as const;
-
-type AppRoutes = (typeof AppRoutes)[keyof typeof AppRoutes];
-
-const routePaths: Record<AppRoutes, string> = {
-  [AppRoutes.HOME]: '/',
-  [AppRoutes.LOGIN]: '/login',
-};
 
 export const routeConfig: RouteProps[] = [
   {
