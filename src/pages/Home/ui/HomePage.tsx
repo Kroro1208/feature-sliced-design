5b35c6b8bdf9794eb873a2458846ac00275dfd29
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-08-21 23:12:36 +0900

    routePaths使用してroute追加

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,5 +1,13 @@
+import { routePaths } from '@/shared/config';
+import { Link } from 'react-router';
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <div>Home Page</div>
+      <Link to={routePaths.login}>Go to Login</Link>
+    </>
+  );
 };
 
 export default HomePage;
--- src/pages/Login/ui/LoginPage.tsx
+++ src/pages/Login/ui/LoginPage.tsx
@@ -1,5 +1,13 @@
+import { routePaths } from '@/shared/config';
+import { Link } from 'react-router';
+
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  return (
+    <>
+      <div>Login Page</div>
+      <Link to={routePaths.home}>Go to Home</Link>
+    </>
+  );
 };
 
 export default LoginPage;
