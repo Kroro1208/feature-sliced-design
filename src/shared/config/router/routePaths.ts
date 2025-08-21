bb1f13030f54af0ed316517f03c522bd71d30e72
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-08-21 23:20:31 +0900

    コメント追加

--- src/shared/config/router/routePaths.ts
+++ src/shared/config/router/routePaths.ts
@@ -5,6 +5,7 @@ export const AppRoutes = {
 
 type AppRoutes = (typeof AppRoutes)[keyof typeof AppRoutes];
 
+// 各pageコンポーネントのLinkでto={routePaths.login}のように使用できる
 export const routePaths: Record<AppRoutes, string> = {
   [AppRoutes.HOME]: '/',
   [AppRoutes.LOGIN]: '/login',
