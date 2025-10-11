5a3cc2bf466ce7e1d5fb5b03f96e9cd4bc4d7da2
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-10-11 22:47:02 +0900

    NFページ実装

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,24 +1,11 @@
-import { Button } from '@/shared/ui';
 import { Footer } from '@/widgets/Footer';
 import { HeaderPage } from '@/widgets/Header';
-import { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
 import styles from './HomePage.module.scss';
 
 const HomePage = () => {
-  const { t } = useTranslation();
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    if (error) throw new Error();
-  }, [error]);
-
   return (
     <div className={styles.pageWrapper}>
       <HeaderPage />
-      <main className={styles.content}>
-        <Button onClick={() => setError(true)}>{t('click')}</Button>
-      </main>
       <Footer />
     </div>
   );
--- src/shared/config/router/routePaths.ts
+++ src/shared/config/router/routePaths.ts
@@ -1,6 +1,7 @@
 export const AppRoutes = {
   HOME: 'home',
   LOGIN: 'login',
+  NOT_FOUND: 'notfound',
 } as const;
 
 type AppRoutes = (typeof AppRoutes)[keyof typeof AppRoutes];
@@ -9,4 +10,5 @@ type AppRoutes = (typeof AppRoutes)[keyof typeof AppRoutes];
 export const routePaths: Record<AppRoutes, string> = {
   [AppRoutes.HOME]: '/',
   [AppRoutes.LOGIN]: '/login',
+  [AppRoutes.NOT_FOUND]: '*',
 };
