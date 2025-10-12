d96173ef01b894276074e866c6284754babcc3dc
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-10-12 21:31:02 +0900

    Input storybook実装

---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { BiSearch } from 'react-icons/bi';
+import { AppIcon } from '../AppIcon/AppIcon';
+import { Input } from './input';
+
+const meta = {
+  title: 'shared/Input ',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  args: { value: '', placeholder: '入力してください' },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    placeholder: 'Default',
+  },
+};
+
+export const WhiteValue: Story = {
+  args: {
+    value: 'white value',
+  },
+};
+
+export const Password: Story = {
+  args: {
+    type: 'password',
+    value: '12345678',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Rounded: Story = {
+  args: {
+    rounded: true,
+  },
+};
+
+export const WithIcon: Story = {
+  args: {
+    Icon: <AppIcon size={20} Icon={BiSearch} />,
+  },
+};
