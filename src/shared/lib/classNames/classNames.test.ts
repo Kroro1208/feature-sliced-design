8db48c2513dc065c2bbf16865a2c4c91a30f5672
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-10-12 20:01:06 +0900

    utility classコンポーネントのテスト実装

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from 'vitest';
+import { classNames } from './className';
+
+describe('classNames', () => {
+  test('一つのクラス引数', () => {
+    expect(classNames('class1')).toBe('class1');
+  });
+  test('複合クラス名', () => {
+    expect(classNames('class1', { class2: true })).toBe('class1 class2');
+    expect(classNames('class1', { class2: false })).toBe('class1');
+  });
+  test('undefined,null,空文字は無視される', () => {
+    expect(classNames('class1', undefined, null, '', 'class2')).toBe(
+      'class1 class2'
+    );
+  });
+});
