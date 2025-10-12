5fe1181e5e79c0520fe502ed7edb4083bc3dbd49
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-10-12 20:13:25 +0900

    Buttonのテスト実装

---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,22 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, test, vi } from 'vitest';
+import { Button } from './Button';
+
+describe('Button', () => {
+  test('render', () => {
+    render(<Button>test</Button>);
+    expect(screen.getByText('test')).toBeInTheDocument();
+  });
+
+  test('onClickが呼ばれること', () => {
+    const handleClick = vi.fn();
+    render(<Button onClick={handleClick}>test</Button>);
+    fireEvent.click(screen.getByText('test'));
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  test('disableが適応されること', () => {
+    render(<Button disabled>test</Button>);
+    expect(screen.getByText('test')).toBeDisabled();
+  });
+});
