ea2da76115d4ed9cf86517862e9437e574064fa7
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-10-18 15:14:17 +0900

    fix: Revert to __dirname for Storybook path resolution

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,12 +1,8 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 import type { StorybookConfig } from '@storybook/react-vite';
 import { mergeConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
